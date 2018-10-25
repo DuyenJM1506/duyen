1f407d4331f98225e1b3c3b3aea2db2416dbb50d
DuyenJM <duyenb1507215@student.ctu.edu.vn>
2018-10-25 19:10:47 +0700

    edit name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NGUYEN YEN DUYEN 
                 </div>
 
                 <div class="links">
