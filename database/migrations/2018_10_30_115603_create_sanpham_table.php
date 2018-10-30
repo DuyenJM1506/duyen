b2a26063429c7ce3f0eb89d98347805d6050f36f
DuyenJM <duyenb1507215@student.ctu.edu.vn>
2018-10-30 19:22:32 +0700

    tao bang loai, chude, sanpham

---
+++ database/migrations/2018_10_30_115603_create_sanpham_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSanphamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sanpham', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigInteger('sp_ma') ->autoIncrement();
+            $table->string ('sp_ten', 191);
+            $table->integer('giaGoc')
+                ->default ('0');
+            $table->unsignedInteger('giaBan')
+                ->default ('0'); 
+            $table->string ('sp_hinh', 200);
+            $table->text('thongTin');
+            $table->string ('sp_danhGia', 50);
+            $table->timestamp('sp_taoMoi') 
+                ->default (DB::raw ('CURRENT_TIMESTAMP')) 
+                ->comment ('Tạo sản phẩm mới'); 
+            $table->timestamp('sp_capNhat') 
+                ->default (DB::raw ('CURRENT_TIMESTAMP')) 
+                ->comment ('Cập nhật sản phẩm mới'); 
+            $table->unsignedTinyInteger('sp_trangThai') 
+                ->default ('2') 
+                ->comment ('Trạng thái sản phẩm: 1-khóa , 2-khả dụng');
+            $table->unsignedTinyInteger ('l_ma')
+                ->comment ('Loại sản phâm');
+            $table->unique('sp_ten');
+
+            $table->foreign('l_ma')
+                ->references ('l_ma')
+                ->on('loai')
+                ->onDelete('CASCADE')
+                ->onUpdate ('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sanpham');
+    }
+}
